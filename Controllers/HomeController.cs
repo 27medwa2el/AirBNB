b1ce0435aaa694300cb1e97499e1ac336b62cb37
27medwa2el <a7med242@gmail.com>
2023-02-21 00:54:47 +0200

    Removed result action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,9 +28,5 @@ namespace AirBNB.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-		public IActionResult result()
-		{
-			return View();
-		}
 	}
 }
