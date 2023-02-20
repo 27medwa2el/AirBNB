6c15f1a3d8d75d3d80d342626952177f4b6ac574
27medwa2el <a7med242@gmail.com>
2023-02-21 00:50:23 +0200

    Added result function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace AirBNB.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+		public IActionResult result()
+		{
+			return View();
+		}
 	}
 }
